68bf0bd48d3e03235fc7c7229d29211a6db3794c
parkjongsoo <newtein80@gmail.com>
2019-03-15 23:20:08 +0900

    wonwooblog 07 test ing...03 not working

--- src/main/java/com/wonwoo/wonwooblog/error/ErrorPageHandler.java
+++ src/main/java/com/wonwoo/wonwooblog/error/ErrorPageHandler.java
@@ -13,11 +13,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
  */
 @Controller
 public class ErrorPageHandler implements ErrorController {
+
     @RequestMapping("/error")
     public String handleError(HttpServletRequest request) {
+
         Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+
         if (status != null) {
+
             int statusCode = Integer.valueOf(status.toString());
+
             if (statusCode == HttpStatus.NOT_FOUND.value()) {
                 // return "error-404";
                 return "error";
@@ -27,6 +32,7 @@ public class ErrorPageHandler implements ErrorController {
                 return "error";
             }
         }
+        
         return "error";
     }
 
